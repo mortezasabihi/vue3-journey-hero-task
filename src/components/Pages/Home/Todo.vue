827953e8284c5d17cca75157fa29f4aebd8cf3f4
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-05-10 12:03:10 +0330

    feat: add todo list to home page

---
+++ src/components/Pages/Home/Todo.vue
@@ -0,0 +1,29 @@
+<template>
+  <VListItem link :to="`/${id}`">
+    <template v-slot:prepend>
+      <VAvatar color="grey-darken-1">
+        <span class="text-h5">{{ id }}</span>
+      </VAvatar>
+    </template>
+
+    <VListItemTitle>{{ title }} - <time class="text-caption">{{ createdAt }}</time></VListItemTitle>
+
+    <VListItemSubtitle>
+      {{ description }}
+    </VListItemSubtitle>
+  </VListItem>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { VListItem, VAvatar, VListItemTitle, VListItemSubtitle } from 'vuetify/components'
+
+interface Props {
+  id: number;
+  title: string;
+  description: string;
+  createdAt: string;
+}
+
+defineProps<Props>()
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,40 @@
 <template>
-  <VContainer>
+  <VContainer class="py-8 px-6 mb-14" fluid>
+    <VRow>
+      <VCol cols="12">
+        <div class="d-flex justify-space-between align-center mb-4">
+          <h2 class="text-h6">List</h2>
 
+          <div class="text-caption">Count: 4</div>
+        </div>
+
+        <VCard>
+          <VList lines="two">
+            <template v-for="n in 6" :key="n">
+              <Todo :title="`Message ${n}`" :id="n"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil repellendus distinctio similique"
+                created-at="Jan 9, 2022" />
+
+              <VDivider v-if="n !== 6" :key="`divider-${n}`" inset />
+            </template>
+          </VList>
+        </VCard>
+      </VCol>
+    </VRow>
   </VContainer>
+
+  <VBtn density="comfortable" icon="mdi-plus" size="x-large" position="absolute" color="blue-darken-1"
+    class="new-todolist-btn" />
 </template>
 
 <script setup lang="ts">
-import { VContainer } from 'vuetify/components'
+import { VContainer, VRow, VCol, VCard, VList, VDivider, VBtn } from 'vuetify/components'
+import Todo from '@/components/Pages/Home/Todo.vue';
 </script>
+
+<style scoped>
+.new-todolist-btn {
+  right: 1rem;
+  bottom: 1rem;
+}
+</style>
