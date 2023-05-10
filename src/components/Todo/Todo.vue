9b83ac55fdc353087b1b08db1ecfce58452457dd
Morteza Sabihi <morteza.sabihi@gmail.com>
2023-05-10 14:10:01 +0330

    feat: add delete button to Todo component

--- src/components/Todo/Todo.vue
+++ src/components/Todo/Todo.vue
@@ -11,12 +11,25 @@
     <VListItemSubtitle>
       {{ description }}
     </VListItemSubtitle>
+
+    <template v-slot:append>
+      <VTooltip text="Delete" location="top">
+        <template v-slot:activator="{ props }">
+          <VBtn v-bind="props" @click.prevent="onDelete" color="red-lighten-1" icon="mdi-trash-can" variant="text"
+            size="small" />
+        </template>
+      </VTooltip>
+    </template>
   </VListItem>
+
+  <VSnackbar v-model="showAlert" :timeout="2000" color="teal" location="bottom left">
+    "{{ title }}" todo deleted
+  </VSnackbar>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue'
-import { VListItem, VAvatar, VListItemTitle, VListItemSubtitle } from 'vuetify/components'
+import { defineProps, ref } from 'vue'
+import { VListItem, VAvatar, VListItemTitle, VListItemSubtitle, VBtn, VTooltip, VSnackbar } from 'vuetify/components'
 
 interface Props {
   id: number;
@@ -25,5 +38,12 @@ interface Props {
   createdAt: string;
 }
 
-defineProps<Props>()
+const props = defineProps<Props>()
+const showAlert = ref(false)
+
+const onDelete = () => {
+  const result = window.confirm(`Do you want to delete "${props.title}" todo?`)
+
+  showAlert.value = result
+}
 </script>
